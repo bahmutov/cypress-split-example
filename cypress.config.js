d97c038f775996960f4ba5126c968ded56157a8d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-01-14 11:31:34 -0500

    split

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,24 @@
+const { findCypressSpecs } = require('find-cypress-specs')
 const { defineConfig } = require('cypress')
+
+function getChunk(values, totalChunks, chunkIndex) {
+  // split all items into N chunks and take just a single chunk
+  if (totalChunks < 0) {
+    throw new Error('totalChunks must be >= 0')
+  }
+
+  if (chunkIndex < 0 || chunkIndex >= totalChunks) {
+    throw new Error(
+      `Invalid chunk index ${chunkIndex} vs all chunks ${totalChunks}`,
+    )
+  }
+
+  const chunkSize = Math.ceil(values.length / totalChunks)
+  const chunkStart = chunkIndex * chunkSize
+  const chunkEnd = chunkStart + chunkSize
+  const chunk = values.slice(chunkStart, chunkEnd)
+  return chunk
+}
 
 module.exports = defineConfig({
   e2e: {
@@ -6,8 +26,20 @@ module.exports = defineConfig({
     supportFile: false,
     fixturesFolder: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      // and load any plugins that require the Node environment
+      if (config.specs) {
+        console.log(config.specs)
+      }
+      if (process.env.SPLIT && process.env.SPLIT_INDEX) {
+        const specs = findCypressSpecs(config)
+        console.log('all specs %d', specs.length)
+        console.log(specs)
+        const splitN = Number(process.env.SPLIT)
+        const splitIndex = Number(process.env.SPLIT_INDEX)
+        console.log('split %d of %d', splitIndex, splitN)
+        const splitSpecs = getChunk(specs, splitN, splitIndex)
+        config.specs = splitSpecs
+        return config
+      }
     },
-  }
+  },
 })
