deacb830c9da8656d781e6e1f8d98a908d70ed02
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-01-16 12:05:20 -0500

    use the cypress-split plugin

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # cypress-split-example ![cypress version](https://img.shields.io/badge/cypress-12.3.0-brightgreen)
 
-> Split Cypress specs on GitHub Actions
+> Split Cypress specs on GitHub Actions and CircleCI
+
+Using the free plugin [cypress-split](https://github.com/bahmutov/cypress-split) without any external services
 
 <!-- prettier-ignore-start -->
-| CI name | CI status |
-| ------- | --------- |
-GitHub Actions | [![ci](https://github.com/bahmutov/cypress-split-example/actions/workflows/ci.yml/badge.svg?branch=main)](https://github.com/bahmutov/cypress-split-example/actions/workflows/ci.yml)
-CircleCI | [![CircleCI](https://dl.circleci.com/status-badge/img/gh/bahmutov/cypress-split-example/tree/main.svg?style=svg&circle-token=dfde227842eaaf720046feeb8ada7cd419732634)](https://dl.circleci.com/status-badge/redirect/gh/bahmutov/cypress-split-example/tree/main)
+CI name | Workflow | CI status
+--- | --- | ---
+GitHub Actions | [ci.yml](./.github/workflows/ci.yml) | [![ci](https://github.com/bahmutov/cypress-split-example/actions/workflows/ci.yml/badge.svg?branch=main)](https://github.com/bahmutov/cypress-split-example/actions/workflows/ci.yml)
+CircleCI | [config.yml](./.circleci/config.yml) | [![CircleCI](https://dl.circleci.com/status-badge/img/gh/bahmutov/cypress-split-example/tree/main.svg?style=svg&circle-token=dfde227842eaaf720046feeb8ada7cd419732634)](https://dl.circleci.com/status-badge/redirect/gh/bahmutov/cypress-split-example/tree/main)
 <!-- prettier-ignore-end -->
--- cypress.config.js
+++ cypress.config.js
@@ -1,30 +1,5 @@
-const { getSpecs } = require('find-cypress-specs')
 const { defineConfig } = require('cypress')
-const ghCore = require('@actions/core')
-const cTable = require('console.table')
-
-function getChunk(values, totalChunks, chunkIndex) {
-  // split all items into N chunks and take just a single chunk
-  if (totalChunks < 0) {
-    throw new Error('totalChunks must be >= 0')
-  }
-
-  if (chunkIndex < 0 || chunkIndex >= totalChunks) {
-    throw new Error(
-      `Invalid chunk index ${chunkIndex} vs all chunks ${totalChunks}`,
-    )
-  }
-
-  const chunkSize = Math.ceil(values.length / totalChunks)
-  const chunkStart = chunkIndex * chunkSize
-  const chunkEnd = chunkStart + chunkSize
-  const chunk = values.slice(chunkStart, chunkEnd)
-  return chunk
-}
-
-const label = 'cypress-split:'
-
-const isDefined = (x) => typeof x !== 'undefined'
+const cypressSplit = require('cypress-split')
 
 module.exports = defineConfig({
   e2e: {
@@ -32,71 +7,9 @@ module.exports = defineConfig({
     supportFile: false,
     fixturesFolder: false,
     setupNodeEvents(on, config) {
-      if (config.specs) {
-        console.log(config.specs)
-      }
-
-      // the user can specify the split flag / numbers
-      // using either OS process environment variables
-      // or Cypress env variables
-      let SPLIT = process.env.SPLIT || config.env.split
-      let SPLIT_INDEX = process.env.SPLIT_INDEX || config.env.splitIndex
-      if (SPLIT === 'true' || SPLIT === true) {
-        // the user wants us to determine the machine index
-        // and the total number of machines, which is possible for some CI systems
-        if (process.env.CIRCLECI) {
-          SPLIT = process.env.CIRCLE_NODE_TOTAL
-          SPLIT_INDEX = process.env.CIRCLE_NODE_INDEX
-          console.log(
-            '%s detected CircleCI machine %d of %d',
-            label,
-            SPLIT,
-            SPLIT_INDEX,
-          )
-        } else {
-          throw new Error('Do not know how to determine the correct split')
-        }
-      }
-
-      if (isDefined(SPLIT) && isDefined(SPLIT_INDEX)) {
-        const specs = getSpecs(config)
-        console.log('%s there are %d found specs', label, specs.length)
-        // console.log(specs)
-        const splitN = Number(SPLIT)
-        const splitIndex = Number(SPLIT_INDEX)
-        console.log('%s split %d of %d', label, splitIndex, splitN)
-        const splitSpecs = getChunk(specs, splitN, splitIndex)
-
-        const specRows = splitSpecs.map((specName, k) => {
-          return [String(k + 1), specName]
-        })
-        console.log(cTable.getTable(['k', 'spec'], specRows))
-
-        if (process.env.GITHUB_ACTIONS) {
-          // https://github.blog/2022-05-09-supercharging-github-actions-with-job-summaries/
-
-          ghCore.summary
-            .addHeading(
-              `${label} chunk ${splitIndex + 1} of ${splitN} (${
-                splitSpecs.length
-              } ${splitSpecs.length === 1 ? 'spec' : 'specs'})`,
-            )
-            .addTable([
-              [
-                { data: 'k', header: true },
-                { data: 'spec', header: true },
-              ],
-              ...specRows,
-            ])
-            .addLink('cypress-split', 'https://cypress.tips')
-            .write()
-        }
-
-        config.specPattern = splitSpecs
-        // TODO: if this is the first machine, it can output
-        // the split chunks to the job summary
-        return config
-      }
+      cypressSplit(on, config)
+      // IMPORTANT: return the config object
+      return config
     },
   },
 })
